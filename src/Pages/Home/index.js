bb953c520b126b8448526e1a2f542de94d0d8f43
Leonardo Henrique Lopes <leonardoo.lopes126@gmail.com>
2020-02-10 23:17:20 -0300

    [20200210] Add profile bar.

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,13 +1,81 @@
 import React from 'react';
 
-import { Container } from './styles';
+import { Container, Profile, Projects, More } from './styles';
+
+import profile from '../../assets/images/profile.jpg';
+
+import email from '../../assets/images/gmail.svg';
+import linkedin from '../../assets/images/din.svg';
+import github from '../../assets/images/git.svg';
+import smartphone from '../../assets/images/smartphone.svg';
+import location from '../../assets/images/location.svg';
 
 import Banner from '../../components/Banner/index';
 
 export default function Home() {
     return (
-        <Container>
+        <>
             <Banner />
-        </Container>
+            <Container>
+                <Profile>
+                    <aside>
+                        <img src={profile} alt="Profile" />
+                        <strong>Leonardo Henrique Lopes</strong>
+                        <span>23 anos.</span>
+                        <span>Desenvolvedor Web, full stack.</span>
+                        <h3>
+                            Olá! Meu nome é Leonardo e eu sou um desenvolvedor
+                            de sistemas e estudante de tecnologia. Sou
+                            apaixonado por desenvolvimento Web e todas as
+                            tecnologias que englobam esse ambiente. Busco sempre
+                            conhecer novas tecnologias e práticas de
+                            desenvolvimento.
+                        </h3>
+                    </aside>
+                    <ul>
+                        <li>
+                            <img src={smartphone} alt="Cellphone" />
+                            (14) 9.8143-4599
+                        </li>
+                        <li>
+                            <img src={location} alt="Location" />
+                            Barra Bonita, SP. Brasil.
+                        </li>
+                        <li>
+                            <img src={email} alt="E-mail" />
+                            leonardoo.lopes126@gmail.com
+                        </li>
+                        <li>
+                            <img src={linkedin} alt="Linkedin" />
+                            <a href="https://www.linkedin.com/in/leonardolopes23">
+                                https://www.linkedin.com/in/leonardolopes23
+                            </a>
+                        </li>
+
+                        <li>
+                            <img src={github} alt="GitHub" />
+                            <a href="https://github.com/lhlleonardo">
+                                https://github.com/lhlleonardo
+                            </a>
+                        </li>
+                    </ul>
+                </Profile>
+                <Projects>
+                    <ul>
+                        <li>
+                            <div>
+                                <strong>Título</strong>
+                                <span>Descrição</span>
+                                <a href="">Clique para acessar!</a>
+                            </div>
+                            <img src="" alt="" />
+                        </li>
+                    </ul>
+                </Projects>
+                <More>
+                    <h1>MAIS</h1>
+                </More>
+            </Container>
+        </>
     );
 }
--- src/Pages/Home/styles.js
+++ src/Pages/Home/styles.js
@@ -1,3 +1,112 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    flex: 0 0;
+`;
+
+export const Profile = styled.div`
+    background: #929aab;
+
+    display: flex;
+    flex-direction: column;
+    align-content: center;
+    align-items: center;
+
+    margin-top: -90px;
+
+    max-width: 320px;
+    height: 600px;
+
+    border-radius: 30px;
+
+    > aside {
+        display: flex;
+        flex-direction: column;
+        align-content: center;
+        align-items: center;
+        justify-content: center;
+
+        img {
+            max-width: 150px;
+            border-radius: 30px;
+            border: 1px solid #d4d7dd;
+            margin-top: 15px;
+        }
+
+        strong {
+            font-size: 18px;
+            color: #000;
+            font-weight: bold;
+            margin-top: 5px;
+        }
+
+        span {
+            font-size: 14px;
+            margin-top: 5px;
+        }
+
+        h3 {
+            font-weight: normal;
+
+            margin-top: 5px;
+            color: #3a3a3a;
+            font-size: 12px;
+            padding: 10px;
+        }
+    }
+
+    ul {
+        list-style: none;
+
+        display: flex;
+        flex-direction: column;
+
+        li {
+            display: flex;
+            flex-direction: row;
+            align-items: baseline;
+            justify-content: left;
+            padding: 5px;
+            font-size: 13px;
+
+            a {
+                text-decoration: none;
+                font-size: 13px;
+                color: #000;
+            }
+
+            img {
+                max-height: 25px;
+                margin-right: 5px;
+            }
+        }
+    }
+`;
+
+export const Projects = styled.div`
+    background: #fff;
+    padding: 15px;
+
+    ul {
+        list-style: none;
+
+        li {
+            strong {
+                font-size: 16px;
+            }
+
+            span {
+                color: #3a3a3a;
+            }
+
+            a {
+                text-decoration: none;
+            }
+        }
+    }
+`;
+
+export const More = styled.div``;
