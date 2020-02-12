b4ac23dab8577d414fb7f4e3baaca92ad820ea7c
Leonardo Henrique Lopes <leonardoo.lopes126@gmail.com>
2020-02-11 22:39:14 -0300

    [20200211] Alter App.js - Footer removed

--- src/App.js
+++ src/App.js
@@ -4,14 +4,12 @@ import { BrowserRouter } from 'react-router-dom';
 import GlobalStyle from './styles/global';
 import Routes from './routes';
 import Header from './components/Header/index';
-import Footer from './components/Footer/index';
 
 function App() {
     return (
         <BrowserRouter>
             <Header />
             <Routes />
-            <Footer />
             <GlobalStyle />
         </BrowserRouter>
     );
